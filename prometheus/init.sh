c19553168aa1882b995e4a61990915567505f701
jjh5887 <wjdrnjsgh159@naver.com>
2025-05-10 20:26:42 +0900

    prometheus: prometheus operator 있을 경우 pull만, apply -> create로 변경 (bug)

--- prometheus/init.sh
+++ prometheus/init.sh
@@ -1,10 +1,23 @@
   #!/bin/bash
 
-git clone https://github.com/prometheus-operator/kube-prometheus.git
+REPO_URL="https://github.com/prometheus-operator/kube-prometheus.git"
+DIR_NAME="kube-prometheus"
+
+if [ -d "$DIR_NAME/.git" ]; then
+    echo "'$DIR_NAME' 디렉토리가 이미 존재합니다. 최신 내용을 가져옵니다..."
+    cd "$DIR_NAME" && git pull
+else
+    echo "'$DIR_NAME' 디렉토리가 존재하지 않습니다. 클론을 수행합니다..."
+    git clone "$REPO_URL" "$DIR_NAME"
+    cd "$DIR_NAME"
+fi
 
 kubectl create ns monitoring
 
-kubectl apply -f manifests/setup
+kubectl create -f manifests/setup
 
-kubectl apply -f manifests
+kubectl create -f manifests
 
+kubectl get crd -n monitoring
+
+kubectl -n monitoring port-forward svc/prometheus-k8s 9090
