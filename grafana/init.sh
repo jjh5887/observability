b01ac63c4ef0046032d4ad920b45a01986b73c79
xEzIxX <by1094@gachon.ac.kr>
2025-05-25 15:58:03 +0900

    grafana: 포트 포워딩 추가

--- grafana/init.sh
+++ grafana/init.sh
@@ -8,4 +8,15 @@ helm repo update
 
 helm upgrade --install --values values.yaml grafana grafana/grafana --namespace grafana
 
-# kubectl port-forward -n grafana svc/grafana 3000:80
+echo "[INFO] Waiting for Grafana pod to be ready..."
+kubectl wait --namespace grafana \
+  --for=condition=Ready pod \
+  --selector=app.kubernetes.io/name=grafana \
+  --timeout=180s
+
+if [ $? -ne 0 ]; then
+  echo "[ERROR] Timed out waiting for Grafana pod to be ready."
+  exit 1
+fi
+
+kubectl port-forward -n grafana svc/grafana 3000:80
